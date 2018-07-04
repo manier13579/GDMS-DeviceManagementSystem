1dcdd6246f92c70a5c85a04bfc199c59a18f7154
Bing Zhe <9061@163.com>
2018-07-04 16:11:50 +0800

    语言管理页面开发

--- GDMS/Controllers/LangManageController.cs
+++ GDMS/Controllers/LangManageController.cs
@@ -20,6 +20,8 @@ namespace GDMS.Controllers
         {
             public string keyword { get; set; }
             public string pageId { get; set; }
+            public int page { get; set; }
+            public int limit { get; set; }
         }
         //返回对象
         private class Response
@@ -29,13 +31,19 @@ namespace GDMS.Controllers
             public string msg { get; set; }
             public object data { get; set; }
         }
+
+        //获取语言列表
+        [ActionName("list")]
         public HttpResponseMessage Lang([FromBody] LangAjax langajax)
         {
             Db db = new Db();
             string where = "";
-            if (langajax.keyword != null) { where = where + "AND ( XU_HAO LIKE '" + langajax.keyword + "' or WEN_ZI LIKE '" + langajax.keyword + "')"; }
-            if (langajax.pageId != null) { where = where + "AND YE_MIAN_MING = '" + langajax.keyword + "'"; }
-            string sql = "select * from GDMS_LANG where 1=1 "+where+" order by YE_MIAN_MING,XU_HAO,YU_ZHONG";
+            if (langajax.keyword != null && langajax.keyword.Length != 0) { where = where + "AND ( XU_HAO LIKE '" + langajax.keyword + "' or WEN_ZI LIKE '" + langajax.keyword + "')"; }
+            if (langajax.pageId != null) { where = where + "AND YE_MIAN_MING = '" + langajax.pageId + "'"; }
+            string sqlnp = "select * from GDMS_LANG where 1=1 "+where+" order by YE_MIAN_MING,XU_HAO,YU_ZHONG ";
+            int limit1 = (langajax.page - 1) * langajax.limit + 1;
+            int limit2 = langajax.page * langajax.limit;
+            string sql = "SELECT * FROM(SELECT p1.*,ROWNUM rn FROM(" + sqlnp + ")p1)WHERE rn BETWEEN "+ limit1 + " AND "+ limit2;
             var ds = db.QueryT(sql);
             Response res = new Response();
             ArrayList data = new ArrayList();
@@ -57,6 +65,33 @@ namespace GDMS.Controllers
                 res.count = col["count"].ToString();
             }
 
+            res.code = 0;
+            res.msg = sql;
+            res.data = data;
+
+            var resJsonStr = JsonConvert.SerializeObject(res);
+            HttpResponseMessage resJson = new HttpResponseMessage
+            {
+                Content = new StringContent(resJsonStr, Encoding.GetEncoding("UTF-8"), "application/json")
+            };
+            return resJson;
+        }
+        //获取select
+        [ActionName("select")]
+        public HttpResponseMessage DeviceSelect()
+        {
+            Db db = new Db();
+            Response res = new Response();
+            ArrayList data = new ArrayList();
+
+            //查询系统select
+            string sql = "select DISTINCT YE_MIAN_MING as PAGE_NAME from GDMS_LANG";
+            var ds = db.QueryT(sql);
+            foreach (DataRow col in ds.Rows)
+            {
+                data.Add(col["PAGE_NAME"].ToString());
+            }
+            
             res.code = 0;
             res.msg = "";
             res.data = data;
